01f98edcc4bef6ef1479d7780fd8e79a8bc17198
99MinSu <dlalstn1023@naver.com>
2024-10-04 20:52:51 +0900

    Initial commit

---
+++ src/main/java/com/example/NextBid/NextBidApplication.java
@@ -0,0 +1,13 @@
+package com.example.NextBid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextBidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextBidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NextBid/NextBidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NextBid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextBidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
